0242143501df67bada45afc6481882a234f4617d
Chief1234 <jukelohnson@gmail.com>
2016-05-14 22:24:27 -0400

    first commit
    basic project setup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float upForce = 2;
+	public float horizontalForce = 2;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, upForce));
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(-horizontalForce, 0));
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalForce, 0));
+		}
+	}
+}
