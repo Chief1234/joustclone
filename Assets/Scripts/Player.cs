4dc55fb8b488585627b03c04332fd13c9de51516
Chief1234 <jukelohnson@gmail.com>
2016-05-19 09:21:40 -0400

    applied new sprites
    applied sprites, roof doesn't do damage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -33,7 +33,7 @@ public class Player : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D c)
     {
-        if (c.relativeVelocity.y > 0)
+        if (c.relativeVelocity.y > 0 && c.collider.gameObject.tag != "NoDamage")
         {
             LoseLife();
         }
